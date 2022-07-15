dd3ea122a40577a33a5a0febfd0811e5df2f7cc9
LogicalAnt <shakil147258@gmail.com>
2022-07-16 02:59:05 +0600

    feat: :sparkles: resolve blocking behavior, show loading spinner

--- utils/getFilesList.js
+++ utils/getFilesList.js
@@ -8,6 +8,10 @@ import head from "lodash/head.js"
 import differenceInSeconds from "date-fns/differenceInSeconds/index.js"
 import childProcess from "child_process"
 import forEach from "lodash/forEach.js"
+import { readdir, stat } from "node:fs/promises"
+import { exec } from "node:child_process"
+import ora from "ora"
+import find from "lodash/find.js"
 
 export const getFilesList = () => {
   const getFilesStatus = () => {
@@ -51,8 +55,44 @@ export const getFilesList = () => {
     return statusList
   }
 
+  const getScriptCommitTimes = async () => {
+    const dates = []
+    const files = await readdir("src/server/")
+    files.forEach((file) => {
+      const datePromise = new Promise((resolve, reject) => {
+        exec(
+          `git log -1 --format=%cd src/server/${file}`,
+          (error, stdout, stderr) => {
+            if (stdout) resolve({ file: file, time: stdout })
+            else if (error) reject(error)
+            else if (stderr) reject(stderr)
+          }
+        )
+      })
+      dates.push(datePromise)
+    })
+
+    return Promise.all(dates)
+  }
+
+  const getCompiledTimes = async () => {
+    const dates = []
+    const files = await readdir("dist/server/")
+
+    files.forEach(async (file) => {
+      const info = stat(`dist/server/${file}`).then((res) => {
+        return { file, time: res.mtime }
+      })
+      const datePromise = new Promise((resolve) => {
+        resolve(info)
+      })
+      dates.push(datePromise)
+    })
+    return Promise.all(dates)
+  }
+
   const getFileInfoTable = async ({ showDate }) => {
-    const data = getFilesStatus()
+    const dateFormat = "dd-MM-yyyy HH:mm:ss a"
     const table = new Table({
       head: [
         "#",
@@ -63,17 +103,45 @@ export const getFilesList = () => {
       colWidths: [5, 42, 8, ...(showDate ? [27, 27] : [])],
     })
 
-    forEach(data, (fileInfo) => {
-      const tableRow = [
-        fileInfo.serial,
-        fileInfo.fileName,
-        fileInfo.synced ? chalk.greenBright("✔") : chalk.redBright("✖"),
-        ...(showDate ? [fileInfo.modified, fileInfo.uploaded] : []),
-      ]
+    const spinner = ora({
+      text: "Loading...\n",
+      spinner: "bouncingBall",
+    }).start()
 
+    const compiledTimes = await getCompiledTimes()
+    const commitTimes = await getScriptCommitTimes()
+
+    forEach(commitTimes, (info, index) => {
+      const compiledFileName = head(get(info, "file", "").split(".")) + ".js"
+      const compiledAt = get(
+        find(compiledTimes, { file: compiledFileName }),
+        "time",
+        null
+      )
+      const committedAt = get(info, "time", null)
+
+      const formattedCompiledDate = compiledAt
+        ? format(new Date(compiledAt), dateFormat)
+        : "-"
+      const formattedCommitDate = committedAt
+        ? format(new Date(committedAt), dateFormat)
+        : "-"
+
+      const syncedStatus =
+        differenceInSeconds(new Date(compiledAt), new Date(committedAt)) >= 0
+          ? true
+          : false
+
+      const tableRow = [
+        index + 1,
+        info.file,
+        syncedStatus ? chalk.greenBright("✔") : chalk.redBright("✖"),
+        ...(showDate ? [formattedCommitDate, formattedCompiledDate] : []),
+      ]
       table.push(tableRow)
     })
-    console.log(table.toString())
+    console.log(table.toString() + "\n")
+    spinner.stop()
   }
 
   return { getFilesStatus, getFileInfoTable }
